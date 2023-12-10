a9be06ef74bd6025fb2de675beda027286aa2ca1
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-10 16:47:38 +0500

    Add project files.

---
+++ LC.Domein/Entities/Courses/Course.cs
@@ -0,0 +1,23 @@
+﻿using LC.Domein.Commons;
+using LC.Domein.Entities.Teachers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC.Domein.Entities.Courses
+{
+    public class Course : Auditable
+    {
+        public string Name { get; set; }
+
+        public string Payment { get; set; }
+
+        public string Duration { get; set; }
+
+        public string Installment_plan { get; set; }
+
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ LC.Domein/Entities/Teachers/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using LC.Domein.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC.Domein.Entities.Teachers
+{
+    public class Teacher : Auditable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string? Experiance { get; set; }
+
+        public string Salary { get; set; }
+    }
+}
