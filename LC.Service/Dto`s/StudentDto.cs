044e33f95e801281745cdbb018fc099de24d319a
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-19 19:31:31 +0500

    add

---
+++ LC.Service/Dto`s/StudentDto.cs
@@ -0,0 +1,32 @@
+﻿using LC.Domein.Entities.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC.Service.Dto_s
+{
+    public class StudentDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string? Address { get; set; }
+
+        public int Payment { get; set; }
+
+        public string Attendance { get; set; }
+
+        public string? ParentsPhoneNumber { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ LC.Service/Dto`s/TeacherDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC.Service.Dto_s
+{
+    public class TeacherDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string? Experiance { get; set; }
+
+        public string Salary { get; set; }
+    }
+}
