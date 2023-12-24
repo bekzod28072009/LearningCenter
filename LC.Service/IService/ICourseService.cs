573803d32e26ee3ffc8ed45307d91f2be645589d
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-24 19:27:44 +0500

    add

---
+++ LC.Service/IService/ICourseService.cs
@@ -0,0 +1,21 @@
+﻿using LC.Domein.Entities.Courses;
+using LC.Domein.Entities.Students;
+using LC.Service.Dto_s;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC.Service.IService
+{
+    public interface ICourseService
+    {
+        IQueryable<Course> GetAll(Expression<Func<Course, bool>> expression, string[] includes = null);
+        ValueTask<CourseDto> GetAsync(Expression<Func<Course, bool>> expression, string[] includes = null);
+        ValueTask<CourseDto> CreateAsync(CourseDto entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<Course, bool>> expression);
+        CourseDto Update(int id, CourseDto entity);
+    }
+}
