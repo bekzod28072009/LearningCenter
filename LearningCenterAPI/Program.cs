194caa7664128d96a1e13ac2d036ced7489e430d
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-24 19:44:50 +0500

    Middleware

--- LearningCenterAPI/Program.cs
+++ LearningCenterAPI/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDBConTextes(builder.Configuration);
-
+builder.Services.AddService();
+builder.Services.AddRepository();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
