b4a703af1ad78aa00ff93d8de455f9944a403ff7
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-25 22:41:20 +0500

    add

---
+++ LearningCenterAPI/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using LC.Service.Dto_s;
+using LC.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService service;
+        public StudentController(IStudentService service)
+        {
+            this.service = service;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(StudentDto dto)
+            => Ok(await service.CreateAsync(dto));
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+            => Ok(service.GetAll(p => p.Id != 0));
+
+
+        [HttpPut("{id}")]
+        public async ValueTask<IActionResult> UpdateAsync([FromRoute] int id, StudentDto dto)
+            => Ok(service.Update(id, dto));
+
+
+
+
+        [HttpGet("{id}")]
+        public async ValueTask<IActionResult> GetAsync([FromRoute] int id)
+            => Ok(await service.GetAsync(u => u.Id == id));
+
+
+        [HttpDelete("{id}")]
+        public async ValueTask<IActionResult> DeleteAsync([FromRoute] int id)
+            => Ok(await service.DeleteAsync(p => p.Id == id));
+    }
+}
