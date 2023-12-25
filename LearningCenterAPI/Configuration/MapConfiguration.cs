6d83522bb437a55178a2f8a7d2c8eea858bd3935
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-25 22:35:02 +0500

    add

---
+++ LearningCenterAPI/Configuration/MapConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LC.Domein.Entities.Courses;
+using LC.Domein.Entities.Students;
+using LC.Domein.Entities.Teachers;
+using LC.Service.Dto_s;
+
+namespace LearningCenterAPI.Configuration
+{
+    public class MapConfiguration : Profile
+    {
+        public MapConfiguration()
+        {
+            CreateMap<Student, StudentDto>().ReverseMap();
+            CreateMap<Teacher, TeacherDto>().ReverseMap();
+            CreateMap<Course, CourseDto>().ReverseMap();
+        }
+    }
+}
